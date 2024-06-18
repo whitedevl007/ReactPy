e8c5bd69fabfc7f64a2a7c2582192fdbb29c0ea8
nizam <officialark11@gmail.com>
2024-06-19 01:45:36 +0530

    added the tasks of Todo List and displayed

--- trycatch.py
+++ trycatch.py
@@ -5,5 +5,26 @@ def Hellocomponent():
     return html.div(
         html.h1("Hello World!")
     )
+
+ 
+@component
+def generateList(items):
+    final_item = [html.li(i) for i in items]
+    return html.ul(final_item)
     
-run(Hellocomponent)
+    
+@component
+def ListComponent():
+    tasks = [
+        'Do yoga',
+        'Drink',
+        'Code',
+        'Sleep'
+    ]
+    
+    return html.div(
+        html.h1("My Todo List !!"),
+        generateList(tasks)
+    )
+    
+run(ListComponent)
